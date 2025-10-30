ab680cf7611228715f01f0586d8bf01c55129f0d
Alastair Mucklow <Alastair.Mucklow@dotdigital.com>
2025-10-30 10:06:09 +0000

    Merged PR 68965: Implement the Dotdigital Tag
    ## What's being changed
    
    We have introduced the DDG proxy tag script.
    We have refactored the email capture scripts
    
    ## Why it's being changed
    
    We want to make all the DDG tag implementations uniform across all the integrations we implement.
    We had the opportunity to remove several redundant files and implementations by refactoring the email capture, improving maintainability and shared functionality.
    
    ## How to review / test this change
    
    - Ensure your Databag keys are enabled.
    - Test page tracking, observe the tracking calls.
    - Test product browsing, observe the tracking calls.
    - Test the newsletter, observe the tracking calls.
    - Test account creation, observe the tracking calls.
    - Smoke test other tracking calls where you see appropriate.
    - **Test the checkout, observe the tracking calls - ensure the Email capture and WBT identify work independently.**
       - Disable WBT by removing the Tracking code leave Email capture enabled: Observe email capture triggered in the network tab tab at checkout.
       - Disable Email capture ensure you have a valid WBT code: Observe only the identify call triggered in the network tab at checkout.
       - Disable Both and observe not network activity related capture and identify
       - Enable both and observe both e-mail capture and identify triggered in the network tab
    - When testing the hyva checkout module with the new tag and capture be sure to disable the `Hyva_CheckoutDotdigitalgroupEmail `
    
    _Hyvä_
    - [X] Email captured from newsletter
    - [X] Email captured at login
    - [X] Email captured at checkout
    
    Related work items: #305092, #315399

---
+++ src/Plugin/EmailCaptureSelectorPlugin.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyva\DotdigitalgroupEmail\Plugin;
+
+use Dotdigitalgroup\Email\Helper\Config;
+use Dotdigitalgroup\Email\Model\Config\Backend\EmailCaptureSelectors;
+use Magento\Config\App\Config\Source\ModularConfigSource;
+use Magento\Framework\Serialize\Serializer\Json;
+
+class EmailCaptureSelectorPlugin
+{
+    /**
+     * @var Json
+     */
+    private $serializer;
+
+    /**
+     * @var ModularConfigSource
+     */
+    private $modularConfigSource;
+
+     /**
+      * @var array
+      */
+     const XML_PATHS_HYVA_DEFAULTS = [
+         'tracking/email_capture_selectors_hyva',
+         'tracking/email_capture_selectors_hyva_checkout'
+     ];
+
+    /**
+     * @param Json $serializer
+     * @param ModularConfigSource $modularConfigSource
+     */
+    public function __construct(
+        Json $serializer,
+        ModularConfigSource $modularConfigSource
+    ) {
+        $this->serializer = $serializer;
+        $this->modularConfigSource = $modularConfigSource;
+    }
+
+    /**
+     * Extend the default email capture selectors when getting configuration value
+     *
+     * @param EmailCaptureSelectors $subject
+     * @param array $result
+     * @return array
+     */
+    public function afterGetConfigurationValue(EmailCaptureSelectors $subject, array $result): array
+    {
+        $defaultConfig = $this->getConfigPath(Config::XML_PATH_CONNECTOR_EMAIL_CAPTURE_SELECTORS);
+        $hyvaConfig = $this->getHyvaConfigurations(static::XML_PATHS_HYVA_DEFAULTS);
+
+        if ($result === $defaultConfig) {
+            return array_merge_recursive($defaultConfig, $hyvaConfig);
+        }
+        return $result;
+    }
+
+    /**
+     * Get configuration value from config path
+     *
+     * @param string $path
+     * @return array
+     */
+    private function getConfigPath(string $path): array
+    {
+        try {
+            return $this->serializer->unserialize(
+                $this->modularConfigSource->get("default/{$path}") ?? ''
+            ) ?? [];
+        } catch (\TypeError $e) {
+            return [];
+        }
+    }
+
+    /**
+     * Get Hyva specific configurations from given config paths
+     *
+     * @param array $hyvaConfigPaths
+     * @return array
+     */
+    private function getHyvaConfigurations(array $hyvaConfigPaths): array
+    {
+        return array_reduce(
+            $hyvaConfigPaths,
+            function ($carry, $configPath) {
+                return array_merge($carry, $this->getConfigPath($configPath));
+            }, []
+        );
+    }
+}
